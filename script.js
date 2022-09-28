94081b879fb18e5245c911515302489b650dd8d7
Peyman Norozi <peyman.norozy@gmail.com>
2022-09-28 03:45:06 -0700

    add the pedometer object

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+let pedometer = {
+  step: 0,
+  increase() {
+    this.step += 1;
+  },
+  decrease() {
+    this.step -= 1;
+  },
+  reset() {
+    this.step = 0;
+  },
+  read() {
+    console.log(this.step);
+  },
+};
