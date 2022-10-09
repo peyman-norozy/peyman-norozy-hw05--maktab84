9b2269a3416f86ba51fc6d4a010aa45952e74999
Peyman Norozi <peyman.norozy@gmail.com>
2022-10-09 02:58:30 -0700

    answer question 5

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const MessageAndLocation = {
-  top: 200,
-  right: 500,
-  html: "Hello!",
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
 };
 
-function showNotification(mal) {
-  const body = document.querySelector("body");
-  body.style.width = "100%";
-  body.style.height = "100vh";
-  const newP = document.createElement("p");
-  newP.innerText = mal.html;
-  body.appendChild(newP);
-  body.style.position = "relative";
-  newP.style.position = "absolute";
-  newP.style.top = String(MessageAndLocation.top) + "px";
-  newP.style.right = String(MessageAndLocation.right) + "px";
-}
-
-showNotification(MessageAndLocation);
-
-console.log(String(MessageAndLocation.top));
+ask(
+  "Do you agree?",
+  () => {
+    alert("You agreed.");
+  },
+  () => {
+    alert("You canceled the execution.");
+  }
+);
